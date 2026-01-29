3bd9c3b04e549dac6d14a82d3f6892b9d74cdb14
John Phillip Lacorte <jaypeelacorte28@gmail.com>
2026-01-29 22:33:22 +0800

    refactor: database url and payment form

--- apps/client/app/components/PaymentForm.tsx
+++ apps/client/app/components/PaymentForm.tsx
@@ -77,10 +77,15 @@ export default function PaymentForm({
               type="number"
               id="amount"
               required
-              min="1"
               value={amount}
+              onKeyDown={(e) => {
+                // Prevent 'e', 'E', '+', '-' from being typed
+                if (["e", "E", "+", "-"].includes(e.key)) {
+                  e.preventDefault();
+                }
+              }}
               onChange={(e) => setAmount(e.target.value)}
-              className="w-full pl-8 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+              className="w-full pl-8 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-black"
               placeholder="0.00"
             />
           </div>
@@ -99,7 +104,7 @@ export default function PaymentForm({
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all text-black"
             placeholder="you@example.com"
           />
         </div>
--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
+import icon from "../public/icon.png";
 import PaymentForm from "./components/PaymentForm";
 
 export default function Home(): React.ReactNode {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div className="text-center">
-          <h1 className="text-3xl font-extrabold text-gray-900">TatakPay</h1>
+      <div className="max-w-md w-full space-y-4">
+        <div className="flex flex-col items-center text-center">
+          <Image src={icon} alt="icon" width={150} height={150} />
+          <h1 className="text-3xl font-extrabold text-gray-900">Tatak-Pay</h1>
           <p className="mt-2 text-sm text-gray-600">
             Welcome to your payment portal
           </p>
